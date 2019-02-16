da55aa8b8690520b5e3324bd2ad4c7348e083548
ivan <ivan@dell.extender.com>
2019-02-16 11:58:06 +0100

    FIrst commit

--- index.php
+++ index.php
@@ -1,23 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Fizz buzz - simple solution</title>
+	<title>FizzBuzz - simple solution</title>
 </head>
 <body>
 	<?php
 		for($i = 1; $i <= 100; $i++) {
-			if($i % 3 == 0 && $i % 5 == 0) {
-				echo "Fizz buzz!<br>";
-			} else {
-				if($i % 3 == 0) {
-					echo "Fizz! <br>";
-				} else if($i % 5 == 0) {
-					echo "Buzz!<br>";
-				} else {
-					echo $i . "<br>";
-				}
+			$str = "<br>";
 
-			}
+			($i % 3 !== 0) ? : $str .= "Fizz";
+			($i % 5 !== 0) ? : $str .= "Buzz";
+
+			($str !== "<br>") ? : $str .= $i;
+
+			echo $str;
 		}
 	?>
 </body>
